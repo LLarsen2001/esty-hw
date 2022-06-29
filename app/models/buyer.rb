aeb91b9a10cc2685570707f7fd89c1285698bbdd
Lance Larsen <lancelarsen2001@gmail.com>
2022-06-29 16:14:07 -0600

    back bug fixes

--- app/models/buyer.rb
+++ app/models/buyer.rb
@@ -1,4 +1,10 @@
 class Buyer < ApplicationRecord
   belongs_to :seller
   serialize :desired_categories, Array
+
+
+  # def yo
+  # self.desired_categories.join('-')
+  # end
+
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,10 @@
 class Product < ApplicationRecord
   belongs_to :seller
+
+  def self.available
+    Product.find_by_sql("SELECT se.name, se.email, p.id, p.price, p.description 
+    FROM products AS p
+    INNER JOIN sellers AS se ON se.id = p.seller_id;")
+  end
 end
+
